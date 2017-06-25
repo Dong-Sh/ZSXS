75eec7ddb73224e53f9e247c40a2f957ee41892a
Dongsh <Dongsh1996@163.com>
2017-06-25 10:21:48 +0800

    D

--- app/src/main/java/com/huida/zsxs/fragment/MainFragment.java
+++ app/src/main/java/com/huida/zsxs/fragment/MainFragment.java
@@ -371,8 +371,9 @@ public class MainFragment extends BaseFragment {
 
                     home_top_rg.check(home_top_rg.getChildAt(home_top_vp.getCurrentItem() % slidesBeanList.size()).getId());
                     break;
-                //
+
             }
         }
     };
+    
 }
